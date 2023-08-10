c161e0a8b0e7fac661abf3b15eb8b1be679362f4
SJS <jinsoo1932@gmail.com>
2023-08-10 15:19:50 +0900

    fo.parkingInfo : 주소 기반 위경도 연동, 예외처리

--- src/main/java/com/cudo/heyparking/parking/agent/service/ParkingServiceImpl.java
+++ src/main/java/com/cudo/heyparking/parking/agent/service/ParkingServiceImpl.java
@@ -6,10 +6,17 @@ import com.cudo.heyparking.util.ResponseCode;
 import com.cudo.heyparking.vo.ParkingVo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -37,6 +44,15 @@ public class ParkingServiceImpl implements ParkingService {
             beforeParkingVo.setParkingName((String) param.get("parkingName"));
             beforeParkingVo.setParkingAddress((String) param.get("parkingAddr"));
             beforeParkingVo.setParkingTotalNumber((String) param.get("parkingTotalNum"));
+
+
+            URI addrToLatLon = urlAddrToLatLon("addrToLatLon", param);
+            log.info("[@External URI>> KAKAO] - {}", addrToLatLon);
+
+            Map<String, Object> restTemplateResponseMap = restTemplateFunction("addrToLatLon", addrToLatLon);
+            Map<String, Object> responseDataMap = (Map<String, Object>) restTemplateResponseMap.get("data");
+            beforeParkingVo.setParkingLat((String) responseDataMap.get("lat"));
+            beforeParkingVo.setParkingLon((String) responseDataMap.get("lon"));
 
             ParkingVo afterParkingVo = parkingRepository.save(beforeParkingVo);
 
@@ -149,4 +165,78 @@ public class ParkingServiceImpl implements ParkingService {
         return resultMap;
     }
 
+
+    public Map<String, Object> restTemplateFunction(String type, URI uri){
+        Map<String, Object> returnMap = new HashMap<>();
+        Map<String, Object> dataMap = new HashMap<>();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "KakaoAK 13cd2d706608b4236574bbd82dba46c6");
+        headers.set("Accept", "*/*;q=0.9"); // HTTP_ERROR 방지
+        HttpEntity<String> httpRequest = new HttpEntity<>(null, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpStatus httpStatus = null;
+        ResponseEntity<Map> httpResponse = null;
+
+        try {
+            httpResponse = restTemplate.exchange(uri, HttpMethod.GET, httpRequest, Map.class);
+            List<Map<String, Object>> responseList = (List<Map<String, Object>>) httpResponse.getBody().get("documents");
+
+            if (responseList.isEmpty()) {
+                throw new Exception("[FAIL][KAKAO SEARCH] - No data available.");
+            }
+
+            if(type.equals("addrToLatLon")) {
+                Map<String, Object> addressInfoLatest = responseList.get(0);
+
+                dataMap.put("lat", addressInfoLatest.get("y"));
+                dataMap.put("lon", addressInfoLatest.get("x"));
+
+                returnMap.put("data", dataMap);
+                returnMap.putAll(ParameterUtils.responseOption(ResponseCode.SUCCESS.getCodeName()));
+            }
+            else{
+                log.info("[FAIL][restTemplate]");
+                returnMap.putAll(ParameterUtils.responseOption(ResponseCode.FAIL.getCodeName()));
+            }
+
+        } catch (Exception exception) {
+            if(exception.getMessage().equals("[FAIL][KAKAO SEARCH] - No data available.")){
+                returnMap.putAll(ParameterUtils.responseOption(ResponseCode.NO_DATA.getCodeName()));
+            }
+            else{
+                returnMap.putAll(ParameterUtils.responseOption(ResponseCode.FAIL.getCodeName()));
+            }
+            log.error("[FAIL][KAKAO SEARCH]", exception.getMessage());
+        }
+        return returnMap;
+    }
+    public URI urlAddrToLatLon(String type, Map<String, Object> data){
+
+        String baseUrl = "https://dapi.kakao.com/v2/local/search/address.json";
+        String analyzeType = "similar";
+        String address = (String) data.get("parkingAddr");
+        String page = "1";
+        String size = "10";
+
+        String encodedAddress = URLEncoder.encode(address, StandardCharsets.UTF_8);
+
+        URI urlBuilder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                .queryParam("analyze_type", analyzeType)
+                .queryParam("query", encodedAddress)
+                .queryParam("page", page)
+                .queryParam("size", size)
+                .build(true)
+                .toUri();
+
+        try {
+            return new URI(urlBuilder.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
